b9cc3f9f1d1f0ccea7274077f38a3c95fc25cb3c
LeonardoNaranjo <47719586+LeonardoNaranjo@users.noreply.github.com>
2022-05-22 19:52:25 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,50 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . # . . #
+        # . . # .
+        . # . . #
+        # . . # .
+        . # . . #
+        `)
+    basic.pause(13000)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(7000)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        . # # # .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(9000)
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        . # # # .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(6000)
+})
